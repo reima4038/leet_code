2be5dcb9c0a66c2cadb97b1aaddcdbcea10d670f
reima4038 <reima4038@gmail.com>
2020-01-08 22:38:44 +0900

    no.5 解答を参考にパフォーマンス改善

--- src/main/java/leet_code/no005/Solution.java
+++ src/main/java/leet_code/no005/Solution.java
@@ -1,78 +1,37 @@
 package leet_code.no005;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
 /**
  * 5. Longest Palindromic Substring
  * https://leetcode.com/problems/longest-palindromic-substring/
  */
 public class Solution {
-    private static final String EMPTY_VALUE = "";
-    private static final int MAX_LENGTH = 1000;
+
+    private static final String EMPTY = "";
 
     public String longestPalindrome(String s) {
-        return Optional.ofNullable(s)
-            .map(action)
-            .orElse(EMPTY_VALUE);
-    }
-
-    /**
-     * 前から読んでも後ろから読んでも同じ場合Trueを返す
-     */
-    private Predicate<String> isPalindrome = s -> {
-        StringBuilder reversalString = new StringBuilder();
-        for(int i = s.length() - 1; i >= 0; i--) {
-            reversalString.append(s.charAt(i));
-        }
-        return Objects.equals(s, reversalString.toString());
-    };
-
-    private Function<List<Character>, String> listToString = h -> {
-        StringBuilder sb = new StringBuilder();
-        for(Character c : h) {
-            sb.append(c);
-        }
-        return sb.toString();
-    };
-
-    private Function<String, String> action = s -> {
-        List<Character> list = new ArrayList<>();
-        String answer = "";
-        int i = 0;
-        int j = 0;
-        while(i < s.length()) {
-            if(i == MAX_LENGTH) {
-                break;
-            } else if(j < s.length() || j == MAX_LENGTH) {
-                list.add(s.charAt(j++));
-                String string = listToString.apply(list);
-                answer = isPalindrome.test(string) ? preferLongLengthValue(answer, string) : answer;
-            } else {
-                list.clear();
-                i++;
-                j = i;
+        if(s == null || s.length() < 1) return EMPTY;
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return answer;
-    };
+        return s.substring(start, end + 1);
+    }
 
-    /**
-     * 与えられた２つの値のうち大きい文字列を返す
-     * @param a 文字列A
-     * @param b 文字列B
-     * @return より長い文字列を返す(同じ長さの場合はaを返す)
-     */
-    private String preferLongLengthValue(String a, String b) {
-        if(Objects.isNull(a) || Objects.isNull(b)) {
-            throw new IllegalArgumentException("input value must not null!");
+    private int expandAroundCenter(String s, int left, int right) {
+        int tempL = left;
+        int tempR = right;
+        while (tempL >= 0 && tempR < s.length() && s.charAt(tempL) == s.charAt(tempR)) {
+            tempL--;
+            tempR++;
         }
-        return a.length() > b.length() ? a : b; 
-    } 
-    
+        return tempR - tempL - 1;
+    }
 
 }
