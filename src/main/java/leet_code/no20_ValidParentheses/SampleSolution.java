ae16003fa365096711665cee0553f5ef7698beda
reima4038 <reima4038@gmail.com>
2020-01-22 23:11:17 +0900

    20. Valid Parentheses: 解答例のタイポ訂正

--- src/main/java/leet_code/no20_ValidParentheses/SampleSolution.java
+++ src/main/java/leet_code/no20_ValidParentheses/SampleSolution.java
@@ -1,12 +1,14 @@
 package leet_code.no20_ValidParentheses;
 
+import java.util.Stack;
+
 /**
  * 20. Valid Parentheses https://leetcode.com/problems/
  */
    public class SampleSolution {
       public boolean isValid(String s) {
          Stack<Character> stack = new Stack<Character>();
-         for(Caharcter c : s.toCharArray()) {
+         for(Character c : s.toCharArray()) {
             if(c == '(') {
                stack.push(')');
             } else if (c == '{') {
